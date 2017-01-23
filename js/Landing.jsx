01ece79d455c9e5d0d6c9ec1fa711d3c15bfc2e6
Bosco <zahiersmith96@gmail.com>
2017-01-23 17:55:54 -0500

    added dynamic placeholder

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -5,21 +5,37 @@ const Landing = React.createClass({
   getInitialState () {
     return {
       searchValue: '',
-      category: 'title'
+      category: 'title',
+      inputPlaceholder: 'The Blacklist'
     };
   },
   handleInput (event) {
     this.setState({searchValue: event.target.value});
   },
   handleSelect (event) {
-    this.setState({category: event.target.value});
+    var value = event.target.value;
+
+    this.setState({category: value});
+
+    //Change place holder based on category
+    switch (value) {
+    case 'title':
+      this.setState({inputPlaceholder: 'The Blacklist'});
+      break;
+    case 'director':
+      this.setState({inputPlaceholder: 'Quentin Tarantino'});
+      break;
+    case 'actor':
+      this.setState({inputPlaceholder: 'Nicolas Cage'});
+      break;
+    }
   },
   render() {
     return (
       <div className="search-box">
           <input
             onChange={this.handleInput} type="text"
-            placeholder="The Blacklist"
+            placeholder={this.state.inputPlaceholder}
             autoFocus/>
           <select name="" id="" onChange={this.handleSelect}>
             <option value="title">Title</option>
