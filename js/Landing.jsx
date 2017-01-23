af3457c7382e12b8fc4749efa7fdba6e6e7f7367
Bosco <zahiersmith96@gmail.com>
2017-01-23 18:50:50 -0500

    chore: refactor

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,57 +1,11 @@
 const React = require('react');
-const { Link } = require('react-router');
+
+const SearchBox = require('./SearchBox');
 
 const Landing = React.createClass({
-  getInitialState () {
-    return {
-      searchValue: '',
-      category: 'title',
-      inputPlaceholder: 'The Blacklist'
-    };
-  },
-  /* COSTOM METHODS */
-  handleInput (event) {
-    this.setState({searchValue: event.target.value});
-  },
-
-  handleSelect (event) {
-    var value = event.target.value;
-
-    this.setState({category: value});
-
-    //Change place holder based on category
-    switch (value) {
-    case 'title':
-      this.setState({inputPlaceholder: 'The Blacklist'});
-      break;
-    case 'director':
-      this.setState({inputPlaceholder: 'Quentin Tarantino'});
-      break;
-    case 'actor':
-      this.setState({inputPlaceholder: 'Nicolas Cage'});
-      break;
-    }
-  },
-  /* ///////////// */
-
-  render() {
+  render () {
     return (
-      <div className="search-box">
-        <form action="">
-          <input
-            onChange={this.handleInput} type="text"
-            placeholder={this.state.inputPlaceholder}
-            autoFocus/>
-          <select name="" id="" onChange={this.handleSelect}>
-            <option value="title">Title</option>
-            <option value="director">Director</option>
-            <option value="actor">Actor</option>
-          </select>
-          <Link  to={`/search/${this.state.category}/${this.state.searchValue}`}>
-            <input className="button" type="submit" value="Search"/>
-          </Link>
-        </form>
-      </div>
+      <SearchBox />
     );
   }
 });
