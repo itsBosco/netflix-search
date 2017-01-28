7460e22dd39a33eefa830bf844171b9f3aa27bfb
Bosco <zahiersmith96@gmail.com>
2017-01-27 20:41:03 -0500

    refactor

--- src/js/DirectorSearch.jsx
+++ src/js/DirectorSearch.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const DirectorSearch = React.createClass({
   render() {
+    let netflixRouletteData = this.props.netflixRouletteData;
     return (
       <div>
-        <pre><code>{JSON.stringify(this.props.netflixRouletteData, null, 4)}</code></pre>
+        <pre><code>{JSON.stringify(netflixRouletteData, null, 4)}</code></pre>
       </div>
 
     );
--- src/js/TitleSearch.jsx
+++ src/js/TitleSearch.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const TitleSearch = React.createClass({
   render() {
+    let netflixRouletteData = this.props.netflixRouletteData;
     return (
       <div>
-        <pre><code>{JSON.stringify(this.props.netflixRouletteData, null, 4)}</code></pre>
-        <img src={this.props.netflixRouletteData.poster} alt=""/>
+        <pre><code>{JSON.stringify(netflixRouletteData, null, 4)}</code></pre>
+        <img src={netflixRouletteData.poster} alt=""/>
       </div>
     );
   }
