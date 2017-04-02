1b6fe87d520c6c696bf711acded80d953421ff4a
Bosco <zahiersmith96@gmail.com>
2017-04-02 16:44:49 -0400

    chore: removed unused file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { HashRouter, Route} from 'react-router-dom';
-import './App.css';
 
 import Landing from './components/Landing/Landing';
 import TitleDetails from './components/TitleDetails/TitleDetails';
